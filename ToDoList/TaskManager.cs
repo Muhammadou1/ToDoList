b9687c21358dfb4da9d32824b9d9653ffed3d0df
Muhammadou Drammeh <mjdrammeh1@bakerripley.org>
2025-06-23 22:58:07 -0500

    Create-App-Framework

---
+++ ToDoList/TaskManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoList
+{
+     public class TaskManager
+    {
+        List<TodoTask> TaskCollection;
+
+        public void CreateTask(string Title)
+        {
+
+        }
+
+        public void DeleteTask(int Id) 
+         {
+
+         }
+
+        public void UpdateStatus()
+        {
+
+        }
+
+        public void ViewTask()
+        {
+
+        }
+
+    }
+}

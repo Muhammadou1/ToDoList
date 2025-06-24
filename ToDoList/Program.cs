89cb2ab90851cd7e9c6ce4a79094fea3821ea292
Muhammadou Drammeh <mjdrammeh1@bakerripley.org>
2025-06-24 16:05:26 -0500

    fixed merge error

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -9,16 +9,16 @@ namespace ToDoList
             Console.WriteLine("\tWelcome to my To-Do List app");
             Console.WriteLine("----------------------------------------------");
            
-            TaskManager taskmanager = new TaskManager();
+            TodoManager taskmanager = new TodoManager();
             Console.WriteLine("What will you like to do?: Type create...");
             string input = Console.ReadLine();
             if(input == "create")
             {
                 string task = Console.ReadLine();
-                taskmanager.CreateTask(task);
+                taskmanager.CreateTodo(task);
                 //string taskindex = taskmanager.TaskCollection[0].Title;
                 Console.WriteLine("Tasks to do");
-                Console.WriteLine(taskmanager.TaskCollection[0].Title);
+                //Console.WriteLine(taskmanager.TodoCollection[0].Title);
             }
 
             

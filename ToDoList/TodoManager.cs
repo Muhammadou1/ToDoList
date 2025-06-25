9e979458d4d0fbffea2212d0a8dff8d9cf65f84b
Muhammadou Drammeh <mjdrammeh1@bakerripley.org>
2025-06-24 22:39:12 -0500

    Added Functions for createTodo method

--- ToDoList/TodoManager.cs
+++ ToDoList/TodoManager.cs
@@ -8,11 +8,16 @@ namespace ToDoList
 {
      public class TodoManager
     {
-        List<TodoItem> TodoCollection;
+        //List<TodoItem> TodoCollection;   ???
+        List<TodoItem> TodoCollection = new List<TodoItem>();  //Adding list into the memory
 
         public void CreateTodo(string Title)
         {
-
+           
+           //TodoCollection = new List<TodoItem>(); ???
+            TodoItem todoItem = new TodoItem();      //Initializing a new instance
+            todoItem.Title = Title;                 //Passing the Title property to the todoItem
+            TodoCollection.Add(todoItem);           //Adding todoItem to the TodoCollection
         }
 
         public TodoItem DeleteTodo(int Id)

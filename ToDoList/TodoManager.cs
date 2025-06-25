9a1fd423a9b8d18de8c9034ca77898505fa6884a
Muhammadou Drammeh <mjdrammeh1@bakerripley.org>
2025-06-25 15:52:05 -0500

    update-todo-items

--- ToDoList/TodoManager.cs
+++ ToDoList/TodoManager.cs
@@ -10,21 +10,29 @@ namespace ToDoList
     {
         List<TodoItem> TodoCollection = new List<TodoItem>();  //Adding list into the memory
         int count = 0;                                         // Increment the Item ID
+       
         public void CreateTodo(string paramTitle)
         {
-            TodoItem todoItem = new TodoItem() { Id = count++, Status = Status.Open(), //Initializing a new instance 
-                                                Title = paramTitle };      
+            TodoItem todoItem = new TodoItem() { Id = count++, Status = Status.Open(),  Title = paramTitle };    //Initializing a new instance with object initialization   
             TodoCollection.Add(todoItem);                      //Adding todoItem to the TodoCollection
         }
+        
         public TodoItem DeleteTodo(int Id)
         {
             return null;
         }
+        
         public void UpdateStatus(int Id, Status Status)
         {
-            
-
+            foreach (var updateItem in TodoCollection)      //Iterate through the entire list
+            {
+                if (updateItem.Id == Id)                    //Check to see if Id property is = to selected Id
+                {
+                    updateItem.Status = Status.Complete();  //Update the status from Open to Complete
+                }
+            }
         }
+       
         public List<TodoItem> GetAllTodoItems()
         {
             return TodoCollection;
